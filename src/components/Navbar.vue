6999e9ec491800614883914366e165e52625852c
drcyph3r <sb4514.work@gmail.com>
2020-10-28 23:35:40 +0530

    ft: primary link in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full border-b border-gray-200 p-4 md:max-w-6xl mx-auto">
     <div class="flex justify-between">
-      <p class="text-lg font-bold">Post.ly</p>
+      <router-link to="/posts" class="text-lg font-bold">Post.ly</router-link>
       <nav class="flex space-x-4">
         <router-link to="/posts">Posts</router-link>
         <div class="cursor-pointer" @click="logout">Logout</div>
