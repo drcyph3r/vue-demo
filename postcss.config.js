e2b333a154cd3f680073840eadec9ef83386c490
drcyph3r <sb4514.work@gmail.com>
2020-10-28 23:30:17 +0530

    ft: boom 🌟

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,3 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.html", "./src/**/*.vue"],
-
-  defaultExtractor: content => {
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-    return broadMatches.concat(innerMatches);
-  }
-});
-
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
+  plugins: [require("tailwindcss"), require("autoprefixer")]
 };
